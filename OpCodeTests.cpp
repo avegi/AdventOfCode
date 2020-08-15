dbe0f72f71006a40ba6827dd775d8f5f53213824
Andras Vegi <andras.vegi@gmail.com>
2020-08-15 22:47:11 +0200

    Tests added

--- OpCodeTests.cpp
+++ OpCodeTests.cpp
@@ -1,16 +1,35 @@
 #include "gtest/gtest.h"
-#include "../Parser.h"
+#include "Parser.h"
 
 TEST(BinaryFunctionsTests, AddTest)
 {
-    std::vector<int> inputCode = {1,0,0,0,2,0,0,0,99};
-    Parser parser = Parser();
-    int t = parser.parse(inputCode, 0);
-    //EXPECT_EQ(parser.parse(inputCode, 0), 4);
+    {
+        std::vector<int> inputCode = {1, 0, 0, 0, 99};
+        Parser parser = Parser();
+        EXPECT_EQ(parser.parse(inputCode, 0), 2);
+    }
+    {
+        std::vector<int> inputCode = {2, 0, 0, 0, 99};
+        Parser parser = Parser();
+        EXPECT_EQ(parser.parse(inputCode, 0), 4);
+    }
+    {
+        std::vector<int> inputCode = {1, 0, 0, 0, 2, 0, 0, 0, 99};
+        Parser parser = Parser();
+        EXPECT_EQ(parser.parse(inputCode, 0), 4);
+    }
+    {
+        std::vector<int> inputCode = {1,1,1,4,99,5,6,0,99};
+        Parser parser = Parser();
+        EXPECT_EQ(parser.parse(inputCode, 4), 2);
+        EXPECT_EQ(parser.parse(inputCode, 0), 30);
+    }
+    {
+        std::vector<int> inputCode = {1,9,10,3,2,3,11,0,99,30,40,50};
+        Parser parser = Parser();
+        EXPECT_EQ(parser.parse(inputCode, 0), 3500);
+    }
 }
-
-
-
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
