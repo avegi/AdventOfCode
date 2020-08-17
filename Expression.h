1b4b23210ba97ecdd582b426e9cd9d8c1c1b1c9b
Andras Vegi <andras.vegi@gmail.com>
2020-08-17 10:18:32 +0200

    Contract added

--- Expression.h
+++ Expression.h
@@ -6,8 +6,7 @@
 
 using namespace std;
 
-/** Expression ABC
- *
+/** Expression Base Class
  */
 class Expression {
 public:
@@ -24,6 +23,8 @@ protected:
     Context &m_input;
 };
 
+/** Base class for expressions with one parameters
+ */
 class OneParamExpression : public Expression{
 public:
     OneParamExpression(Context &input, std::vector<string> mask);
@@ -31,6 +32,8 @@ protected:
     int m_expressionPos;
 };
 
+/** Base class for expressions with two parameters
+ */
 class TwoParamsExpression : public Expression{
 public:
     TwoParamsExpression(Context &input, std::vector<string> mask);
@@ -40,6 +43,8 @@ protected:
     int m_outPos;
 };
 
+/** Base class for expressions with three parameters
+ */
 class ThreeParamsExpression : public Expression{
 public:
     ThreeParamsExpression(Context &input, std::vector<string> mask);
@@ -50,8 +55,9 @@ protected:
     int m_outPos;
 };
 
-/**
- *
+/** Add expression
+ *  Adds the first two parameters (param1 + param2).
+ *  Stores the result at the location marked by the third parameter.
  */
 class AddExpression : public ThreeParamsExpression{
 public:
@@ -59,8 +65,9 @@ public:
     int process() override;
 };
 
-/**
- *
+/** Multiply expression
+ *  Multiplies the first two parameters (param1 * param2).
+ *  Stores the result at the location marked by the third parameter.
  */
 class MultiplyExpression : public ThreeParamsExpression{
 public:
@@ -69,38 +76,62 @@ public:
 
 };
 
-
-
+/** Input expression
+ *  Reads the standard input and stores the value marked the expression parameter.
+ */
 class InputExpression : public OneParamExpression{
 public:
     InputExpression(Context &input, std::vector<string> mask);
     void writeData(int inValue) override;
 };
 
+/** Output expression
+ * Returns the value stored at the expression parameter.
+ */
 class OutputExpression : public OneParamExpression{
 public:
     OutputExpression(Context &input, std::vector<string> mask);
     int readData() override;
 };
 
+/** Jump if True expression
+ * Checks if the value at first parameter is not zero (param1 != 0).
+ * Jumps to position marked by the second parameter if true.
+ * Does nothing otherwise.
+ */
 class JumpIfTrueExpression : public TwoParamsExpression{
 public:
     JumpIfTrueExpression(Context &input, std::vector<string> mask);
     int process() override;
 };
 
+/** Jump if False expression
+ * Checks if the value at first parameter is zero (param1 == 0).
+ * Jumps to position marked by the second parameter if true.
+ * Does nothing otherwise.
+ */
 class JumpIfFalseExpression : public TwoParamsExpression{
 public:
     JumpIfFalseExpression(Context &input, std::vector<string> mask);
     int process() override;
 };
 
+/** Less Than expression
+ * Compares the first two parameters (param1 < param2).
+ * Stores 1 at the location marked by the third parameter if true.
+ * Stores 0 otherwise.
+ */
 class LessThanExpression : public ThreeParamsExpression{
 public:
     LessThanExpression(Context &input, std::vector<string> mask);
     int process() override;
 };
 
+/** Equals expression
+ * Compares the first two parameters (param1 == param2).
+ * Stores 1 at the location marked by the third parameter if true.
+ * Stores 0 otherwise.
+ */
 class EqualsExpression : public ThreeParamsExpression{
 public:
     EqualsExpression(Context &input, std::vector<string> mask);
